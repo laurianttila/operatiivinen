9461d1dc8e3d97e71b49841f95e0b1c30d2f8e00
Lauri Anttila <lauri.anttila@gmail.com>
2024-12-04 14:11:39 +0200

    Update config.js

--- config.js
+++ config.js
@@ -4,6 +4,7 @@ const config = {
     'https://junalahdot.fi/tampere?command=fs&id=462&dt=dep&lang=1',
     'https://www.lounaat.info/lounas/linkosuo-min-asemakeskus/tampere',
     'https://www.lounaat.info/lounas/aitoleipa-tammela-go21/tampere',
-    'https://tremonitori.digitransit.fi/view?cont=UunOME8G5DrXeGtaJqgJDg=='
+    'https://tremonitori.digitransit.fi/view?cont=UunOME8G5DrXeGtaJqgJDg==',
+    'https://embed.waze.com/iframe?zoom=14&lat=61.485267&lon=23.768063&ct=livemap'
   ]
 };
